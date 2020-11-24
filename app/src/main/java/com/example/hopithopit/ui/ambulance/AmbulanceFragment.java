ae1033a335441e0be685a5d10c1812f0209bb444
alextn00 <39369516+alextn00@users.noreply.github.com>
2020-11-24 20:15:58 +0900

    Ambulance

--- app/src/main/java/com/example/hopithopit/ui/ambulance/AmbulanceFragment.java
+++ app/src/main/java/com/example/hopithopit/ui/ambulance/AmbulanceFragment.java
@@ -1,10 +1,19 @@
 package com.example.hopithopit.ui.ambulance;
 
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,22 +24,155 @@ import androidx.lifecycle.ViewModelProviders;
 import com.example.hopithopit.R;
 import com.example.hopithopit.ui.search.SearchViewModel;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import jxl.read.biff.BiffException;
+
 public class AmbulanceFragment extends Fragment {
 
     private AmbulanceViewModel ambulanceViewModel;
+    private Spinner spinner1;
+    private Spinner spinner2;
+
+    //선택된 대구 경북 시구군
+    private String select_dg;
+    private String select_gb;
+
+    ListView listView;
+    ArrayAdapter <String> arrayAdapter;
+
+    /*public final List<dg_ambulance> dg_ambul_list = new ArrayList<dg_ambulance>();
+    public final List<gb_ambulance> gb_ambul_list = new ArrayList<gb_ambulance>();*/
+
+    String path = AmbulanceFragment.class.getResource("").getPath();
+    File dg_file = new File(path + "dg_private_ambulance");
+    File gb_file = new File(path + "gb_private_ambulance");
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ambulanceViewModel =
                 ViewModelProviders.of(this).get(AmbulanceViewModel.class);
         View root = inflater.inflate(R.layout.fragment_ambulance, container, false);
-        final TextView textView = root.findViewById(R.id.text_ambulance);
+
+        spinner1 = (Spinner)root.findViewById(R.id.spinner1);
+        spinner2 = (Spinner)root.findViewById(R.id.spinner2);
+
+        final String[] dosi={"대구", "경북"};
+        final String[] dg={"남구","달서구","동구","북구","서구","수성구","중구","달성군"};
+        final String[] gb={"경산시","경주시","고령군","구미시","군위군","김천시", "문경시",
+                "봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시",
+                "예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군","포항시"};
+
+        //지역 조건 선택하는 spinner
+        final ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,dosi);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(adapter1.getItem(i).equals("대구")){
+                    final ArrayAdapter<String> adapter2=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_dropdown_item_1line, dg);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner2.setAdapter(adapter2);
+                    spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            select_dg= (String) adapterView.getItemAtPosition(i);
+                        }
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                }
+                else{
+                    final ArrayAdapter<String> adapter2=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_dropdown_item_1line, gb);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner2.setAdapter(adapter2);
+                    spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            select_gb= (String) adapterView.getItemAtPosition(i);
+                            //txt_room.setText(select_gb);
+                        }
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        Button button = (Button)root.findViewById(R.id.ambulance_search);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(select_dg.equals("달서구")){
+
+                }
+            }
+        });
+
+        listView = (ListView)root.findViewById(R.id.ambulance_list);
+        readExcel();
+
+        /*final TextView textView = root.findViewById(R.id.text_ambulance);
         ambulanceViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
         return root;
     }
+
+    private void readExcel() {
+        /*try {
+            AssetManager am = getResources().getAssets();
+            InputStream is = null;
+            try{
+                is = am.open("dg_private_ambulance.xls");
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+            Workbook wb = Workbook.getWorkbook(is);
+
+            if(wb != null) {
+                Sheet sheet = wb.getSheet(0);   // 시트 불러오기
+                if(sheet != null) {
+                    int colTotal = sheet.getColumns();    // 전체 컬럼
+                    int rowIndexStart = 1;                  // row 인덱스 시작
+                    int rowTotal = sheet.getColumn(colTotal-1).length;
+
+                    StringBuilder sb;
+                    for(int row=rowIndexStart;row<rowTotal;row++) {
+                        sb = new StringBuilder();
+                        for(int col=0;col<colTotal;col++) {
+                            String contents = sheet.getCell(col, row).getContents();
+                            sb.append("col"+col+" : "+contents+" , ");
+                        }
+                        Log.i("test", sb.toString());
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        }*/
+    }
+
 }
