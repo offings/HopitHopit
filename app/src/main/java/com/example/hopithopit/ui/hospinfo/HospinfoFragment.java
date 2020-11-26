c0ca07b6a63730ec076ad07bfe6fb10a26ae80ca
dnbi <51847115+dnbi@users.noreply.github.com>
2020-11-27 02:26:16 +0900

    Update MainActivity.java, Create HospInfoList
    fragment간 이동 추가, 병원정보 목록 recyclerView 추가

--- app/src/main/java/com/example/hopithopit/ui/hospinfo/HospinfoFragment.java
+++ app/src/main/java/com/example/hopithopit/ui/hospinfo/HospinfoFragment.java
@@ -19,7 +19,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.example.hopithopit.MainActivity;
 import com.example.hopithopit.R;
+
+import java.time.Instant;
 
 public class HospinfoFragment extends Fragment {
 
@@ -27,16 +30,7 @@ public class HospinfoFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        hospinfoViewModel =
-                ViewModelProviders.of(this).get(HospinfoViewModel.class);
         View root = inflater.inflate(R.layout.fragment_hospinfo, container, false);
-        final TextView textView = root.findViewById(R.id.text_hospinfo);
-        hospinfoViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
 
         final GridView gridView = root.findViewById(R.id.hospinfoGridView);
         GridViewAdapter gridViewAdapter = new GridViewAdapter();
@@ -93,7 +87,8 @@ public class HospinfoFragment extends Fragment {
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Toast.makeText(context, hospinfo, Toast.LENGTH_SHORT).show();
+                    //Toast.makeText(context, hospinfo, Toast.LENGTH_SHORT).show();
+                    ((MainActivity)getActivity()).replaceFragment(R.id.action_navigation_hospinfo_to_hospinfoListFragment);
                 }
             });
 
