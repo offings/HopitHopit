34e4492b4f4156bffe65fe6ec64f435490757812
alextn00 <alextn@naver.com>
2020-11-28 16:37:06 +0900

    Ambulance db

--- app/src/main/java/com/example/hopithopit/ui/ambulance/AmbulanceFragment.java
+++ app/src/main/java/com/example/hopithopit/ui/ambulance/AmbulanceFragment.java
@@ -1,12 +1,18 @@
 package com.example.hopithopit.ui.ambulance;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -18,6 +24,7 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hopithopit.R;
 import com.example.hopithopit.ui.search.SearchViewModel;
+import com.example.hopithopit.ui.ambulance.AmbulanceViewModel;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -33,65 +40,105 @@ public class AmbulanceFragment extends Fragment {
     private Spinner spinner2;
 
     //선택된 대구 경북 시구군
-    private String select_dg;
-    private String select_gb;
+    private String select_dg_gb;
 
     ListView listView;
     ArrayAdapter<String> arrayAdapter;
+    SQLiteDatabase db;
+    DBHelper helper;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         ambulanceViewModel =
                 ViewModelProviders.of(this).get(AmbulanceViewModel.class);
         View root = inflater.inflate(R.layout.fragment_ambulance, container, false);
-        /*final TextView textView = root.findViewById(R.id.text_ambulance);
-        ambulanceViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        spinner1=(Spinner)root.findViewById(R.id.spinner1);
+        spinner2=(Spinner)root.findViewById(R.id.spinner2);
+
+        final String[] dosi={"대구", "경북"};
+        final String[] dg={"남구","달서구","동구","북구","서구","수성구","중구","달성군"};
+        final String[] gb={"경산시","경주시","고령군","구미시","군위군","김천시", "문경시",
+                "봉화군","상주시","성주군","안동시","영덕군","영양군","영주시","영천시",
+                "예천군","울릉군","울진군","의성군","청도군","청송군","칠곡군","포항시"};
+
+        //지역 조건 선택하는 spinner
+        final ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,dosi);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });*/
-
-        listView = (ListView)root.findViewById(R.id.ambulance_list);
-        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
-        readExcel();
-        return root;
-    }
-
-    public void readExcel() {
-        Workbook workbook = null;
-        String contents = null;
-        try {
-            InputStream is = getActivity().getResources().getAssets().open("dg_ambulance.xls");
-            Workbook wb = Workbook.getWorkbook(is);
-
-            if(wb != null) {
-                Sheet sheet = wb.getSheet(0);   // 시트 불러오기
-                if(sheet != null) {
-                    int colTotal = sheet.getColumns();    // 전체 컬럼
-                    int rowIndexStart = 1;                  // row 인덱스 시작
-                    int rowTotal = sheet.getColumn(colTotal-1).length;
-
-                    StringBuilder sb;
-                    for(int row=rowIndexStart;row<rowTotal;row++) {
-                        sb = new StringBuilder();
-                        for(int col=0;col<colTotal;col++) {
-                            contents = sheet.getCell(col, row).getContents();
-                            sb.append("col"+col+" : "+contents+" , ");
-
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                if(adapter1.getItem(i).equals("대구")){
+                    final ArrayAdapter<String> adapter2=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_dropdown_item_1line, dg);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner2.setAdapter(adapter2);
+                    spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            select_dg_gb= (String) adapterView.getItemAtPosition(i);
                         }
-                        Log.i("test", sb.toString());
-                        arrayAdapter.add(contents);
-                    }
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
+                }
+                else if (adapter1.getItem(i).equals("경북")){
+                    final ArrayAdapter<String> adapter2=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_dropdown_item_1line, gb);
+                    adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    spinner2.setAdapter(adapter2);
+                    spinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                            select_dg_gb= (String) adapterView.getItemAtPosition(i);
+                        }
+                        @Override
+                        public void onNothingSelected(AdapterView<?> adapterView) {
+                        }
+                    });
                 }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (BiffException e) {
-            e.printStackTrace();
-        }finally {
-            listView.setAdapter(arrayAdapter);
-            //workbook.close();
-        }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        listView = (ListView)root.findViewById(R.id.ambulance_list);
+//        arrayAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1);
+
+        Button button = (Button)root.findViewById(R.id.ambulance_search);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadDB();
+            }
+        });
+        return root;
+
     }
+
+    private void loadDB() {
+        String sql = null;
+        Cursor cursor = null;
+        helper = new DBHelper(getActivity());
+        db = helper.getReadableDatabase();
+        helper.onCreate(db);
+
+        if(select_dg_gb.equals("달서구")){
+            sql = String.format("select type, num, tel from ambulance where district == '달서구'",0);
+            cursor = db.rawQuery(sql, null);
+            String[] strs = new String[]{"txt"};
+            int[] ints = new int[]{R.id.ambulance_list};
+
+            SimpleCursorAdapter adapter = null;
+            adapter = new SimpleCursorAdapter(listView.getContext(), R.layout.ambulance_list, cursor, strs, ints, 0);
+
+            listView.setAdapter(adapter);
+        }
+
+    }
+
+
 }
---
+++ app/src/main/java/com/example/hopithopit/ui/ambulance/DBHelper.java
@@ -0,0 +1,61 @@
+package com.example.hopithopit.ui.ambulance;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    final  String TAG = "DBHelper";
+
+    public Context context;
+
+    public static final String TABLE_NAME = "ambulance";
+    public static final String ambulance_own = "own";
+    public static final String ambulance_type = "type";
+    public static final String ambulance_num = "num";
+    public static final String ambulance_tel = "tel";
+    public static final String ambulance_city = "city";
+    public static final String ambulance_district = "district";
+
+    static  final String DB_NAME = "ambulance.db";
+    static  final int DB_VERSION = 1;
+
+    public DBHelper(Context context){
+        super(context, DB_NAME, null, DB_VERSION);
+        this.context = context;
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        StringBuffer stringBuffer = new StringBuffer();
+        stringBuffer.append("Create table " + TABLE_NAME + "(");
+        stringBuffer.append(ambulance_own + " TEXT, ");
+        stringBuffer.append(ambulance_type + " TEXT, ");
+        stringBuffer.append(ambulance_num + " TEXT, ");
+        stringBuffer.append(ambulance_tel + " TEXT, ");
+        stringBuffer.append(ambulance_city + " TEXT, ");
+        stringBuffer.append(ambulance_district + " TEXT ");
+
+        Log.d(TAG, stringBuffer.toString());
+        db.execSQL(stringBuffer.toString());
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        try{
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /*public Cursor getAllData(String sql){
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(sql, null);
+        return cursor;
+    }*/
+}
