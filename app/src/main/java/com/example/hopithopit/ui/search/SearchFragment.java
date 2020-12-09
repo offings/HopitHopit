9eba895fd6ead0e720ef222be36e61ea19d71b48
alextn00 <alextn@naver.com>
2020-12-09 20:01:27 +0900

    update search

--- app/src/main/java/com/example/hopithopit/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/hopithopit/ui/search/SearchFragment.java
@@ -13,23 +13,38 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hopithopit.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class SearchFragment extends Fragment {
 
     private SearchViewModel searchViewModel;
+    private MapView mapView = null;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
         View root = inflater.inflate(R.layout.fragment_search, container, false);
-        final TextView textView = root.findViewById(R.id.text_search);
-        searchViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        mapView = (MapView)root.findViewById(R.id.map);
+       // mapView.getMapAsync(this);
         return root;
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+
+        LatLng SEOUL = new LatLng(37.56, 126.97);
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(SEOUL);
+        markerOptions.title("서울");
+        markerOptions.snippet("수도");
+        googleMap.addMarker(markerOptions);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+
     }
 }
