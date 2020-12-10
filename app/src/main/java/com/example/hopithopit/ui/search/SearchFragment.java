5d4b922b2725615fbeb7480f757ae6789357ab14
dnbi <51847115+dnbi@users.noreply.github.com>
2020-12-10 21:56:21 +0900

    Update SearchFragment.java

--- app/src/main/java/com/example/hopithopit/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/hopithopit/ui/search/SearchFragment.java
@@ -4,47 +4,32 @@ import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.hopithopit.MainActivity;
 import com.example.hopithopit.R;
-import com.example.hopithopit.ui.search.SearchViewModel;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
@@ -56,17 +41,12 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.io.InputStreamReader;
 import java.io.StringReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 public class SearchFragment extends Fragment implements OnMapReadyCallback {
 
@@ -76,15 +56,39 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
     private FragmentActivity mContext;
     private Marker currentMarker = null;
     public static String search="";
-    public String qn = "";
 
-    private Location mCurrentLocatiion;
+    public static Location mlocation = null;
 
-    String dg_search_api = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncListInfoInqire?serviceKey=yNTJ6AknFLjMpn1Bme5Rk6Rr1Piz57T4zyDXLp7MfYFXgsOnojoMBpujFVSTkODNcAk1O3dWCtOiZIW%2F%2BKVFPg%3D%3D&Q0=%EB%8C%80%EA%B5%AC%EA%B4%91%EC%97%AD%EC%8B%9C&QN=";
-    String gb_search_api = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncListInfoInqire?serviceKey=yNTJ6AknFLjMpn1Bme5Rk6Rr1Piz57T4zyDXLp7MfYFXgsOnojoMBpujFVSTkODNcAk1O3dWCtOiZIW%2F%2BKVFPg%3D%3D&Q0=%EA%B2%BD%EC%83%81%EB%B6%81%EB%8F%84&QN=";
+    String cur_search_api = "http://apis.data.go.kr/B552657/HsptlAsembySearchService/getHsptlMdcncLcinfoInqire?serviceKey=yNTJ6AknFLjMpn1Bme5Rk6Rr1Piz57T4zyDXLp7MfYFXgsOnojoMBpujFVSTkODNcAk1O3dWCtOiZIW%2F%2BKVFPg%3D%3D&WGS84_LON=127.085156592737&WGS84_LAT=37.4881325624879&pageNo=1&numOfRows=100";
 
     private static final String KEY_CAMERA_POSITION = "camera_position";
     private static final String KEY_LOCATION = "location";
+
+    static final Integer APP_PERMISSION = 1;
+
+    private void askForPermission(String permission, Integer requestCode){
+        if (ContextCompat.checkSelfPermission(getActivity(), permission)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(), permission)) {
+                ActivityCompat.requestPermissions(getActivity(), new String[] {permission }, requestCode);
+            } else {
+                ActivityCompat.requestPermissions(getActivity(), new String[] {permission }, requestCode);
+            }
+        } else {
+            //Toast.makeText(getContext(), "" + permission + " is already granted", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if(ActivityCompat.checkSelfPermission(getActivity(), permissions[0]) == PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(getContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     public void onAttach(Activity activity) {
         mContext = (FragmentActivity) activity;
@@ -101,30 +105,56 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
         searchViewModel =
                 ViewModelProviders.of(this).get(SearchViewModel.class);
         if (savedInstanceState != null) {
-            mCurrentLocatiion = savedInstanceState.getParcelable(KEY_LOCATION);
+            mlocation = savedInstanceState.getParcelable(KEY_LOCATION);
             CameraPosition mCameraPosition = savedInstanceState.getParcelable(KEY_CAMERA_POSITION);
         }
 
         final View root = inflater.inflate(R.layout.fragment_search, container, false);
 
-        EditText input_search = (EditText)root.findViewById(R.id.search);
-        qn = input_search.getText().toString();
+        askForPermission(Manifest.permission.ACCESS_FINE_LOCATION, APP_PERMISSION);
+        LocationManager lm = (LocationManager)getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener(){
+
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                mlocation = location;
+                DownloadWebpageTask downloadWebpageTask = new DownloadWebpageTask();
+                downloadWebpageTask.execute(cur_search_api.replace("WGS84_LON=127.085156592737", "WGS84_LON="+mlocation.getLongitude()).replace("WGS84_LAT=37.4881325624879", "WGS84_LAT="+mlocation.getLatitude()));
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(@NonNull String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(@NonNull String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED)
+            return null;
+
+        if (lm.getAllProviders().contains(LocationManager.NETWORK_PROVIDER))
+            lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 100, locationListener);
+
+        if (lm.getAllProviders().contains(LocationManager.GPS_PROVIDER))
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 100, locationListener);
+
 
         ImageView btn_search = (ImageView)root.findViewById(R.id.btn_search);
 
-        //button안눌러도 뜨기
-       /* btn_search.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    search_api(dg_search_api, qn);
-                    search_api(gb_search_api, qn);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-*/
         mapView = (MapView) root.findViewById(R.id.mapFragment);
         if (mapView != null) {
             mapView.onCreate(savedInstanceState);
@@ -157,36 +187,21 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
         return root;
     }
 
-    public void search_api(String api, String searchQN) throws IOException {
-        StringBuilder urlBuilder = new StringBuilder(api);
-        urlBuilder.append(URLEncoder.encode(searchQN, "UTF-8"));
-        urlBuilder.append("&pageNo=1&numOfRows=10");
-//        Toast.makeText(getContext(), searchQN, Toast.LENGTH_SHORT).show();
-
-        DownloadWebpageTask downloadWebpageTask = new DownloadWebpageTask();
-        downloadWebpageTask.execute(urlBuilder.toString());
-    }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        LatLng SEOUL = new LatLng(37.56, 126.97);
+        LatLng curPos = new LatLng(mlocation.getLatitude(), mlocation.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
-        markerOptions.position(SEOUL);
-        markerOptions.title("서울");
-        markerOptions.snippet("수도");
+        markerOptions.position(curPos);
+        markerOptions.title("현재위치");
         googleMap.addMarker(markerOptions);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(SEOUL));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(curPos));
         googleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
 
-        try {
-            search_api(dg_search_api, qn);
-            search_api(gb_search_api, qn);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
+        DownloadWebpageTask downloadWebpageTask = new DownloadWebpageTask();
+        downloadWebpageTask.execute(cur_search_api.replace("WGS84_LON=127.085156592737", "WGS84_LON="+curPos.longitude).replace("WGS84_LAT=37.4881325624879", "WGS84_LAT="+curPos.latitude));
 
     }
 
@@ -268,10 +283,10 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
 
-            boolean indutyAddr = false; boolean indutyName = false; boolean indutyTel1 = false; boolean indutyTel3 = false;
-            boolean inwgs84Lon = false; boolean inwgs84Lat = false;
-            String dutyAddr = ""; String dutyName = ""; String dutyTel1 = ""; String dutyTel3 = "";
-            double wgs84Lon = 0.0d; double wgs84Lat = 0.0d;
+            boolean indutyAddr = false; boolean indutyName = false; boolean indutyTel1 = false;
+            boolean indistance = false; boolean inlongitude = false; boolean inlatitude = false;
+            String dutyAddr = ""; String dutyName = ""; String dutyTel1 = "";
+            double distance = 0.0d; double longitude = 0.0d; double latitude = 0.0d;
 
             try {
                 XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
@@ -292,12 +307,12 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
                             indutyName = true;
                         } else if (xpp.getName().equals("dutyTel1")){
                             indutyTel1 = true;
-                        } else if (xpp.getName().equals("dutyTel3")){
-                            indutyTel3 = true;
-                        } else if (xpp.getName().equals("wgs84Lon")){
-                            inwgs84Lon = true;
-                        } else if (xpp.getName().equals("wgs84Lat")){
-                            inwgs84Lat = true;
+                        } else if (xpp.getName().equals("distance")){
+                            indistance = true;
+                        } else if (xpp.getName().equals("longitude")){
+                            inlongitude = true;
+                        } else if (xpp.getName().equals("latitude")){
+                            inlatitude = true;
                         }
                     } else if (eventType == XmlPullParser.TEXT) {
                         if (indutyAddr){
@@ -309,30 +324,35 @@ public class SearchFragment extends Fragment implements OnMapReadyCallback {
                         } else if (indutyTel1){
                             dutyTel1 = xpp.getText();
                             indutyTel1 = false;
-                        } else if (indutyTel3){
-                            dutyTel3 = xpp.getText();
-                            indutyTel3 = false;
-                        } else if (inwgs84Lon){
-                            wgs84Lon = Double.parseDouble(xpp.getText());
-                            inwgs84Lon = false;
-                        } else if (inwgs84Lat){
-                            wgs84Lat = Double.parseDouble(xpp.getText());
-                            inwgs84Lat = false;
+                        } else if (indistance){
+                            distance = Double.parseDouble(xpp.getText());
+                            indistance = false;
+                        } else if (inlongitude){
+                            longitude = Double.parseDouble(xpp.getText());
+                            inlongitude = false;
+                        } else if (inlatitude){
+                            latitude = Double.parseDouble(xpp.getText());
+                            inlatitude = false;
                         }
                     } else if (eventType == XmlPullParser.END_TAG) {
                         if (xpp.getName().equals("item")) {
-                            LatLng hlatlng = new LatLng(wgs84Lat, wgs84Lon);
+                            LatLng hlatlng = new LatLng(latitude, longitude);
                             MarkerOptions markerOptions = new MarkerOptions();
                             markerOptions.position(hlatlng);
                             markerOptions.title(dutyName);
                             markerOptions.snippet(dutyAddr);
                             mMap.addMarker(markerOptions);
-                            mMap.moveCamera(CameraUpdateFactory.newLatLng(hlatlng));
-                            mMap.animateCamera(CameraUpdateFactory.zoomTo(10));
                         }
                     }
                     eventType = xpp.next();
                 }
+                LatLng curPos = new LatLng(mlocation.getLatitude(), mlocation.getLongitude());
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(curPos);
+                markerOptions.title("현재위치");
+                mMap.addMarker(markerOptions);
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(curPos));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(13));
             } catch (Exception e) {
 
             }
